ca2714c874ebf8c4949f5deba0f5b77efaadcbbb
Jitto P.Jose <jittopjose@gmail.com>
2026-01-04 21:43:15 -0500

    initial commit

---
+++ ionic-app/vite.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest" />
+
+import legacy from '@vitejs/plugin-legacy'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  base: './',
+  build: {
+    outDir: '../web',
+    emptyOutDir: true,
+    assetsDir: '',
+    cssCodeSplit: false,
+    modulePreload: {
+      polyfill: false
+    },
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        manualChunks: undefined,
+        entryFileNames: 'app.js',
+        assetFileNames: 'app.[ext]'
+      }
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
+})
